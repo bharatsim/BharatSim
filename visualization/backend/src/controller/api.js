d73006be024137768c18e3a47c760a226ffd04d0
gauravd <gauravd@thoughtworks.com>
2020-07-01 14:38:02 +0530

    000| removed unnecesary api route |Gaurav, Ganesh

--- visualization/backend/src/controller/api.js
+++ visualization/backend/src/controller/api.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const csvService = require ('../services/csvService.js')
 
-router.get('/', (req, res) => res.send('Hello World! Welcome'))
 
 router.get('/data', function (req, res) {
     const {columns} = req.query;
