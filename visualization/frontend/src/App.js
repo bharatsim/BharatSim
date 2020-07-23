bb4bd6ba342d09233308ef20ccbb4538279e1e03
Ganesh Kathar <ganesh.kathar@thoughtworks.com>
2020-07-23 12:43:08 +0530

    BHA-28 | Fix title alignment| Ganesh

--- visualization/frontend/src/App.js
+++ visualization/frontend/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
+import { Typography } from '@material-ui/core';
 import DashboardLayout from './component/dashboardLayout/DashboardLayout';
 
 function App() {
   return (
     <>
-      Welcome to BharatSim Visualization
-      <DashboardLayout />
+      <Box pt={2} pb={2}>
+        <Typography variant="h3" align="center">
+          Welcome to BharatSim Visualization
+        </Typography>
+        <DashboardLayout />
+      </Box>
     </>
   );
 }
--- visualization/frontend/src/__tests__/App.test.js
+++ visualization/frontend/src/__tests__/App.test.js
@@ -5,9 +5,9 @@ import App from '../App';
 jest.mock('../component/dashboardLayout/DashboardLayout', () => () => <>Dash Board Layout</>);
 
 describe('<App />', () => {
-  it('renders learn react link', () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/Welcome to BharatSim Visualization/i);
-    expect(linkElement).toBeInTheDocument();
+  it('should match snapshot', () => {
+    const { container } = render(<App />);
+
+    expect(container).toMatchSnapshot();
   });
 });
