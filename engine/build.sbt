dfaaae5d22d1f64cc6a3d50f474f1ddec448a075
gaurav <gauravd@thoughtworks.com>
2020-08-11 15:21:25 +0530

    BHA-64| initial setup for engine | Gaurav

---
+++ engine/build.sbt
@@ -0,0 +1,5 @@
+name := "engine"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
---
+++ engine/src/main/scala/com/bharatsim/engine/HelloWord.scala
@@ -0,0 +1,8 @@
+package com.bharatsim.engine
+
+object HelloWord {
+
+  def main(args: Array[String]): Unit = {
+    print("Hello world!!")
+  }
+}
