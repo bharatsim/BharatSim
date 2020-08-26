56e4bfad498856979ed86d6b627cb1424c61136f
Chandrkant Kaski <chandrkantkaski@Chandrkants-MacBook-Pro.local>
2020-08-26 14:32:56 +0530

    Setup |Add Http-Logger to the api | Chandrakant

--- visualization/backend/app.js
+++ visualization/backend/app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const multer = require('multer');
+const morgan = require('morgan');
 
+const LOGGER_FORMAT_STRING = ':method :url :status :param[id] :res[content-length] - :response-time ms';
 const apiRoutes = require('./src/controller/api.js');
 require('./setupDB');
 
 const FILE_UPLOAD_PATH = './uploads/';
 
+morgan.token('param', function (req, res, param) {
+  return req.params[param];
+});
+
 const app = express();
+
+app.use(morgan(LOGGER_FORMAT_STRING));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(multer({ dest: FILE_UPLOAD_PATH }).single('datafile'));
