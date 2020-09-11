e1ecce3c64fc3e0f70e152a2613ed12b8ff5746a
Swapnil Khandekar <swapnikh@thoughtworks.com>
2020-09-12 01:13:45 +0530

    Revert "BHA-83 | Added simulation model with COVID specific disease dynamics | Jayanta"
    This reverts commit fdeb2f9d87abefd65045aea3b008dc951110514b.

--- engine/src/main/scala/com/bharatsim/model/Disease.scala
+++ engine/src/main/scala/com/bharatsim/model/Disease.scala
@@ -3,17 +3,5 @@ package com.bharatsim.model
 import com.bharatsim.engine.Dynamics
 
 object Disease extends Dynamics {
-  final val infectionRate: Double = 0.5
-
-  final val asymptomaticPercentage: Double = 0.3
-  final val mildSymptomaticPercentage: Double = 0.3
-
-  final val transmissionStartDay: Int = 2
-  final val exposedDuration: Int = 2
-  final val preSymptomaticDuration: Int = 2
-  final val lastDay: Int = 3
-  final val asymptomaticLastDay: Int = 9
-  final val mildSymptomaticLastDay: Int = 12
-
-  final val deathRate = 0.02
+  val infectionRate = 10
 }
--- engine/src/main/scala/com/bharatsim/model/Main.scala
+++ engine/src/main/scala/com/bharatsim/model/Main.scala
@@ -6,7 +6,7 @@ import scala.util.Random
 object Main {
   def steUpContext(context: Context): Unit = {
 
-    val numberOfCitizen = 10
+    val numberOfCitizen = 10;
     val house = new House
 
     for (i <- 1 to numberOfCitizen) {
@@ -15,22 +15,20 @@ object Main {
       context.agents.add(citizen)
       house.addMember(citizen)
     }
-    context.simulationContext.setSteps(100)
+    context.simulationContext.setSteps(10)
     context.setDynamics(Disease)
   }
 
   def main(args: Array[String]): Unit = {
     val context = new Context
     steUpContext(context)
-    val totalInfectedBeforeSimulation =
+    val beforeCount =
       context.agents.getAll.count(_.asInstanceOf[Citizen].isInfected)
     Simulation.run(context)
-
-    val totalRecovered =
-      context.agents.getAll.count(_.asInstanceOf[Citizen].isRecovered)
-    println("#Infected before simulation: " + totalInfectedBeforeSimulation)
-
-    println("#Recovered after simulation: " + totalRecovered)
+    val afterCount =
+      context.agents.getAll.count(_.asInstanceOf[Citizen].isInfected)
+    println(beforeCount)
+    println(afterCount)
   }
 
 }
