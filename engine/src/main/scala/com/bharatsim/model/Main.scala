20f4062b474c238d451aca82fb244b6fcdd966b8
KJAYANTA <jayantak@thoughtworks.com>
2020-09-25 17:45:37 +0530

    BHA-83 | Refactor, moved logic into separate method | Jayanta

--- engine/src/main/scala/com/bharatsim/model/Main.scala
+++ engine/src/main/scala/com/bharatsim/model/Main.scala
@@ -9,17 +9,38 @@ import scala.util.Random
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val simulationContext = new SimulationContext(10)
+    val simulationContext = new SimulationContext(1000)
     implicit val context: Context = Context(Disease, simulationContext)
 
+    ingestDataUsingCSV(context)
+
+//    ingestData()
+    val beforeCount = context.graphProvider.fetchNodes("Citizen", ("infectionState", Infected)).size
+    context.registerAgent[Citizen]
+
+    Simulation.run(context)
+
+    val afterCountSusceptible = context.graphProvider.fetchNodes("Citizen", ("infectionState", Susceptible)).size
+    val afterCountInfected = context.graphProvider.fetchNodes("Citizen", ("infectionState", Infected)).size
+    val afterCountRecovered = context.graphProvider.fetchNodes("Citizen", ("infectionState", Recovered)).size
+    val afterCountDeceased = context.graphProvider.fetchNodes("Citizen", ("infectionState", Deceased)).size
+
+    println("Infected before: " + beforeCount)
+    println("Infected after: " + afterCountInfected)
+    println("Recovered: " + afterCountRecovered)
+    println("Deceased: " + afterCountDeceased)
+    println("Susceptible: " + afterCountSusceptible)
+  }
+
+  private def ingestDataUsingCSV(context: Context): Unit = {
     context.graphProvider.ingestFromCsv(
       "src/main/resources/citizen.csv",
       Some(value = (map: Map[String, String]) => {
-        val params = new mutable.HashMap[String, Any];
+        val params = new mutable.HashMap[String, Any]
         params.put("infectionState", InfectionStatus.withName(map("infectionState")))
         params.put("age", map("age").toInt)
         val citizenNode = new DataNode {
-          override def label: String = map("label");
+          override def label: String = map("label")
 
           override def Id: NodeId = map("id").toInt
 
@@ -37,24 +58,13 @@ object Main {
         GraphData(List(citizenNode, homeNode), List(staysAt, memberOf))
       })
     )
-
-//    ingestData()
-    val beforeCount = context.graphProvider.fetchNodes("Citizen", ("infectionState", Infected)).size
-    context.registerAgent[Citizen]
-
-    Simulation.run(context)
-
-    val afterCount = context.graphProvider.fetchNodes("Citizen", ("infectionState", Infected)).size
-
-    println(beforeCount)
-    println(afterCount)
   }
 
   private def ingestData()(implicit context: Context): Unit = {
     val numberOfCitizen = 100
     val houseId = context.graphProvider.createNode("Home")
 
-    for (i <- 1 to numberOfCitizen) {
+    for (_ <- 1 to numberOfCitizen) {
       val infectionState = if (Random.nextBoolean()) Infected else Susceptible
       val agentId = context.graphProvider.createNode("Citizen", ("infectionState", infectionState))
       context.graphProvider.createRelationship(agentId, "STAYS_AT", houseId)
