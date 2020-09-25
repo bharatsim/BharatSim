df8b9b64d18435217d5e8e4cf2f98ff763dcd571
KJAYANTA <jayantak@thoughtworks.com>
2020-09-25 13:35:04 +0530

    BHA-83 | Added disease states(SEIR) to COVID disease model | Jayanta

--- engine/src/main/scala/com/bharatsim/model/Disease.scala
+++ engine/src/main/scala/com/bharatsim/model/Disease.scala
@@ -3,5 +3,8 @@ package com.bharatsim.model
 import com.bharatsim.engine.Dynamics
 
 object Disease extends Dynamics {
-  val infectionRate = 10
+  final val infectionRate: Double = 0.5
+  final val exposedDuration: Int = 2
+  final val lastDay: Int = 17
+  final val deathRate = 0.02
 }
--- engine/src/main/scala/com/bharatsim/model/InfectionStatus.scala
+++ engine/src/main/scala/com/bharatsim/model/InfectionStatus.scala
@@ -2,5 +2,5 @@ package com.bharatsim.model
 
 object InfectionStatus extends Enumeration {
   type InfectionStatus = Value
-  val Susceptible, Infected, Recovered = Value
+  val Susceptible, Exposed, Infected, Recovered, Deceased = Value
 }
