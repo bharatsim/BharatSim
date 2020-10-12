7060cb395a50f8e39b3b59004f523036e6e8680e
Chandrakant Kaski <kaskichandrakant@gmail.com>
2020-10-12 17:41:44 +0530

    BHA-35|create clickable card ui component| Chandrakant/Ganesh

---
+++ visualization/frontend/src/uiComponent/ClickableCard.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import { ChildrenPropTypes } from '../commanPropTypes';
+
+const useStyles = makeStyles((theme) => {
+  return {
+    ClickableCardContainer: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      border: '1px dashed',
+      borderColor: theme.colors.primaryColorScale['200'],
+      '&:hover': {
+        backgroundColor: theme.colors.primaryColorScale['50'],
+        cursor: 'pointer',
+      },
+    },
+  };
+});
+
+function ClickableCard({ children, onClick }) {
+  const classes = useStyles();
+  return (
+    <Box onClick={onClick} className={classes.ClickableCardContainer}>
+      {children}
+    </Box>
+  );
+}
+
+ClickableCard.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: ChildrenPropTypes.isRequired,
+};
+
+export default ClickableCard;
