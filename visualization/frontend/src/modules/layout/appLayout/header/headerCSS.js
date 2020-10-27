416cfd35aa28ce4554b7cdfd43ff14e20532bd49
Chandrakant Kaski <kaskichandrakant@gmail.com>
2020-10-27 18:24:35 +0530

    BHA-QA-106/35 |fix extra ponter on header and add install scripts| Chandrakant/ganesh

--- visualization/frontend/src/modules/layout/appLayout/header/headerCSS.js
+++ visualization/frontend/src/modules/layout/appLayout/header/headerCSS.js
@@ -5,11 +5,11 @@ export default makeStyles((theme) => ({
     display: 'flex',
     height: theme.spacing(16),
     background: theme.colors.primaryColorScale[700],
-    cursor: 'pointer',
   },
   logo: {
     display: 'flex',
     alignItems: 'center',
     padding: theme.spacing(3, 0, 3, 6),
+    cursor: 'pointer',
   },
 }));
