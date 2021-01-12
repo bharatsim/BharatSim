c14c44ee2fc19e6ba04ea63dadc4d1d9501e4b31
Swapnil Khandekar <swapnikh@thoughtworks.com>
2021-01-12 11:53:24 +0530

    BHA-163 | Add API documentation for per tick cache | Swapnil

--- src/main/scala/com/bharatsim/engine/cache/PerTickCache.scala
+++ src/main/scala/com/bharatsim/engine/cache/PerTickCache.scala
@@ -2,7 +2,15 @@ package com.bharatsim.engine.cache
 
 import scala.collection.mutable
 
-class PerTickCache(store: mutable.HashMap[Any, Any] = mutable.HashMap.empty) {
+class PerTickCache(private val store: mutable.HashMap[Any, Any] = mutable.HashMap.empty) {
+  /**
+   * This function tries to retrieve value from the cache
+   * If value is absent from the cache, it evaluates the valueFunction and puts the value in cache
+   *
+   * @param key           Key for which value to be fetched
+   * @param valueFunction Function that returns value if value not present in cache
+   * @return Value from either cache or the function
+   */
   def getOrUpdate(key: Any, valueFunction: () => Any): Any = {
     if (store.contains(key)) store(key)
     else {
@@ -12,14 +20,31 @@ class PerTickCache(store: mutable.HashMap[Any, Any] = mutable.HashMap.empty) {
     }
   }
 
+  /**
+   * Puts value in cache
+   *
+   * @param key   Key for which value needs to be put
+   * @param value value against the key
+   */
   def put(key: Any, value: Any): Unit = {
     store.put(key, value)
   }
 
+  /**
+   * If key is present returns Some(value) else None
+   *
+   * @param key Key for which value needs to be fetched
+   * @return Optional value
+   */
   def get(key: Any): Option[Any] = {
     store.get(key)
   }
 
+  /**
+   * Erases the value from the store
+   *
+   * @param key key for which value needs to be erased
+   */
   def erase(key: Any): Unit = {
     store.remove(key)
   }
